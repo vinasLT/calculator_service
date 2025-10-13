014006593d79a4448afcc4c2bcd80b92dfc1f081
ivanpetrovskij <peyrovskaaa@gmai.com>
2025-10-14 00:10:08 +0200

    exchange rate and private routes update

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-public_v1_router = APIRouter(prefix='/public/v1')
+public_v1_router = APIRouter(prefix='/v1/public')
 private_v1_router = APIRouter(prefix='/private/v1')
 
 
--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 import uvicorn
 from fastapi import FastAPI
 
-from app.api.api_v1.api import private_v1_router, public_v1_router
+from app.api.api_v1.api import public_v1_router
 from app.core.app_factory import create_app
 
 app: FastAPI = create_app()
 
+app = FastAPI()
+
+app.include_router(public_v1_router)
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8000)
